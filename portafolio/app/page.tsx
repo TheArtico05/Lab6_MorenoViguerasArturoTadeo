85d69282ce0ee9cc1e2482c9da5fef1330c99d48
Arturo Moreno <morenoviguerasarturotadeo@gmail.com>
2026-04-19 20:47:08 +0000

    Creacion de la parte interest

--- portafolio/app/page.tsx
+++ portafolio/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Header from "./Components/Header/Header";
 import HeroSection from "./Components/HeroSection/Hero.section";
+import Interest from "./Components/Interest/Interest";
 
 export default function Home() {
   return (
     <><Header/>
-     <HeroSection/>
+      <HeroSection/>
+      <Interest/>
     </>
   );
 }
